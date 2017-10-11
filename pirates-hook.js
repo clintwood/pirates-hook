457a2856b1c3ddc12d866fc34b6f79b3bbb44d49
clintwood (Office) <clint@anotherway.co.za>
2017-10-11 12:53:13 +0200

    remove debugger statements - log registration

--- pirates-hook.js
+++ pirates-hook.js
@@ -1,14 +1,14 @@
 /* eslint no-console:0 */
 const pirates = require('pirates');
-debugger;
 
 function matcher() {
   return true;
 }
 
+console.log('Registering pirates-hook...');
 pirates.addHook((code, filename) => {
-  debugger;
   console.log('filename', filename);
   console.log('code', code);
   return code;
 }, { exts: ['.js', '.mjs'], matcher });
+console.log('Registered pirates-hook.');
